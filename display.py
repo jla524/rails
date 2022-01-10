4f9d9e618f96217dfb85f5e6da4bb739889c50bc
Jacky Lee <jla524@sfu.ca>
2022-01-09 21:01:02 -0800

    Lane detection working for country road

---
+++ display.py
@@ -0,0 +1,107 @@
+import sys
+import cv2
+import numpy as np
+
+
+def grey(image):
+    #convert to grayscale
+    image = np.asarray(image)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+
+#Apply Gaussian Blur --> Reduce noise and smoothen image
+def gauss(image):
+    return cv2.GaussianBlur(image, (5, 5), 0)
+
+
+#outline the strongest gradients in the image --> this is where lines in the image are
+def canny(image):
+    edges = cv2.Canny(image,50,150)
+    return edges
+
+
+def region(image):
+    height, width = image.shape
+    #isolate the gradients that correspond to the lane lines
+    p1 = (width // 10, height)
+    p2 = (width // 5 * 3, height // 2)
+    p3 = (width, height)
+    triangle = np.array([[p1, p2, p3]])
+    #create a black image with the same dimensions as original image
+    mask = np.zeros_like(image)
+    #create a mask (triangle that isolates the region of interest in our image)
+    mask = cv2.fillPoly(mask, triangle, 255)
+    mask = cv2.bitwise_and(image, mask)
+    return mask
+
+
+def display_lines(image, lines):
+    lines_image = np.zeros_like(image)
+    #make sure array isn't empty
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line
+            #draw lines on a black image
+            cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return lines_image
+
+
+def average(image, lines):
+    left = []
+    right = []
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            #fit line to points, return slope and y-int
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            y_int = parameters[1]
+            #lines on the right have positive slope, and lines on the left have neg slope
+            if slope < 0:
+                left.append((slope, y_int))
+            else:
+                right.append((slope, y_int))
+    #takes average among all the columns (column0: slope, column1: y_int)
+    right_avg = np.average(right, axis=0)
+    left_avg = np.average(left, axis=0)
+    # TODO: average can be NaN
+    #create lines based on averages calculates
+    left_line = make_points(image, left_avg)
+    right_line = make_points(image, right_avg)
+    return np.array([left_line, right_line])
+
+
+def make_points(image, average):
+    slope, y_int = average
+    y1 = image.shape[0]
+    #how long we want our lines to be --> 3/5 the size of the image
+    y2 = int(y1 * (3/5))
+    #determine algebraically
+    x1 = int((y1 - y_int) // slope)
+    x2 = int((y2 - y_int) // slope)
+    return np.array([x1, y1, x2, y2])
+
+
+def process(image):
+    edges = cv2.Canny(image, 50, 150)
+    isolated = region(edges)
+    lines = cv2.HoughLinesP(isolated, 2, np.pi/180, 100, np.array([]), minLineLength=30, maxLineGap=5)
+    averaged_lines = average(image, lines)
+    black_lines = display_lines(image, averaged_lines)
+    lanes = cv2.addWeighted(image, 0.8, black_lines, 1, 1)
+    return lanes
+
+
+if __name__ == '__main__':
+    path = sys.argv[1] if len(sys.argv) > 1 else 'videos/test_countryroad.mp4'
+    cap = cv2.VideoCapture(path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow('Frame', process(frame))
+        # Press q to exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
